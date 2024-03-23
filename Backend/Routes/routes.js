750b4a165708ee183f1880e847f189ec7b70f1bc
ODaniel235 <domole235@gmail.com>
2024-03-23 18:30:35 +0100

    clearing backend

--- Backend/Routes/routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { signup, login } from "../Controllers/authController.js";
-import { getAllUsers } from "../Controllers/UsersController.js";
-router.post("/signup", signup);
-router.get("/signup", getAllUsers);
-router.post("/login", login);
-export default router;
