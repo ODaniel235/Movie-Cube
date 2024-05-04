c0bcbc8a1793c39aaf836918ecfb0e64f8f83c02
Omole Oluwadamilare Daniel <domole235@gmail.com>
2024-05-04 20:07:59 +0100

    customizing alert function

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,11 +1,18 @@
 import Swal from "sweetalert2";
 
-export default function Alert(displayIcon, response, responseText){
-    Swal.fire({
-        icon: displayIcon,
-        title: response,
-        text: responseText,
-        background: 'black',
-        backdrop: 'blur 200px'
-    })
+export default function Alert(displayIcon, response, responseText) {
+  Swal.fire({
+    icon: displayIcon,
+    title: response,
+    text: responseText,
+    background: "#121212",
+    confirmButtonColor: "#e50914",
+    iconColor: "#e50914",
+    customClass: {
+      title: "text-white",
+      content: "text-white",
+      confirmButton: "bg-red-700 text-white",
+    },
+    backdrop: "rgba(0, 0, 0, 0.8)",
+  });
 }
